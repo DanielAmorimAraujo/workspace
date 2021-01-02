c35da6e040e2fcede56f41cfb722a62d1a062b67
Daniel Araujo <daniel.amorim.araujo@gmail.com>
2021-01-01 21:35:59 -0500

    Change name from Form1 to Create

--- Workspace/Create.cs
+++ Workspace/Create.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Workspace
 {
-    public partial class Form1 : Form
+    public partial class Create : Form
     {
-        public Form1()
+        public Create()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
